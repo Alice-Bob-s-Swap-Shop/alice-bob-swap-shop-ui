1a4e710ad920e493c781d83c2298c49e9282ed22
Azaro <qppazaro@gmail.com>
2022-04-28 21:13:59 +0300

    Added moralis

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,20 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { MoralisProvider } from "react-moralis";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <MoralisProvider
+      serverUrl="https://t5sltoahuxi9.usemoralis.com:2053/server"
+      appId="iKhBPtyG6n4w84nFI4xsWh4Pj4GDUI76Px0mek8v"
+    >
       <Head>
         <title>Alice & Bob's Swap Shop</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </MoralisProvider>
   );
 }
 
