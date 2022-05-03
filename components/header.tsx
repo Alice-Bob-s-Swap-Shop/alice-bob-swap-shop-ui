32fab7a22077a4209b50f2a8da2664a3fe85282f
Azaro <qppazaro@gmail.com>
2022-05-03 16:01:36 +0300

    Added moralis auth.

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useMoralis } from "react-moralis";
+
+const getAddressAlias = (ethAddress: string): string => {
+  return ethAddress.replace(
+    ethAddress.substring(6, ethAddress.length - 4),
+    "..."
+  );
+};
 
 const Header: React.FC = () => {
+  const { authenticate, isAuthenticated, user } = useMoralis();
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      return;
+    }
+    setName(getAddressAlias(user!.get("ethAddress")));
+  }, [isAuthenticated]);
+
+  const login = async () => {
+    if (isAuthenticated) {
+      return;
+    }
+
+    await authenticate({ signingMessage: "Log in using Moralis" })
+      .then(function (user) {
+        console.log("logged in user:", user);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   return (
     <div className="bg-slate-900/10">
       <header className="container flex justify-between py-4 items-center">
@@ -16,7 +48,9 @@ const Header: React.FC = () => {
           </Link>
         </div>
 
-        <button className="btn">Connect</button>
+        <button type="button" className="btn" onClick={login}>
+          {isAuthenticated ? name : "Connect"}
+        </button>
       </header>
     </div>
   );
